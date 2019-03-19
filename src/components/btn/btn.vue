d6f7f0c295ee17542f90a3837eb066232eef54e1
lhyu <lhyu606@163.com>
2019-03-19 23:25:04 +0800

    progress state changes

---
+++ src/components/btn/btn.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="btn" :class="'btn'+type" @click='goto'>
+    {{ text }}
+  </div>
+</template>
+
+<script>
+
+// type 按钮状态
+// 1 主色
+// 2 辅助色
+export default {
+  name: 'botton',
+  props: {
+  	text: {
+      type: String,
+      default: '1'
+    },
+  	type: {
+      type: Number,
+      default: 3
+    },
+    url: {
+      type: String,
+      default: '/home'
+    }
+  },
+  data() {
+    return {
+      
+    }
+  },
+  created() {
+    // console.log(this.text, this.type);
+  },
+  methods: {
+    goto() {
+      this.$router.push(this.url)
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.btn {
+	width: 356px;
+	height: 56px;
+  line-height: 56px;
+  font-size: 20px;
+  letter-spacing: 2px;
+  color: white;
+  text-align: center;
+  margin: 0 auto;
+}
+.btn1 {
+	border: 1px solid $btn-orange;
+	background: $btn-orange;
+}
+.btn2 {
+	border: 1px solid $btn-grey;
+	background: transparent;
+}
+</style>
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="home">
     <div class='mask'>
-      <div class="nav" @click='showSide'>
-        <div class='line'></div>
-        <div class='line'></div>
-        <div class='line'></div>
+      <div class="pdinner">
+        <div class="nav" @click='showSide'>
+          <div class='line'></div>
+          <div class='line'></div>
+          <div class='line'></div>
+        </div>
+        <div class='content'>
+          <h1 class='hi'>嗨，</h1>
+          <div v-if='state == 0' class="text">
+            <p>欢迎预约有间餐厅</p>
+          </div>
+          <div v-else class="text">
+            <p>感谢</p>
+            <p>预约有间餐厅</p>
+            <p>您预约了</p>
+            <!-- <p class='time'><span class='time'>{{  }} 月 {{  }} 日的{{  }}</span></p>-->
+            <p><span class='time'>2月17日的午餐</span></p>
+            <p>主厨静候您的光临</p>
+          </div>
+        </div>
       </div>
-      <div class='content'>
-        <h1 class='hi'>嗨，</h1>
-        <div v-if='state == 0'>
-          <p>欢迎预约有间餐厅</p>
-        </div>
-        <div v-else>
-          <p>感谢</p>
-          <p>预约有间餐厅</p>
-          <p>您预约了</p>
-          <!-- <p class='time'>{{  }}月{{  }}日的{{  }}</p>-->
-          <p class='time'>2 月 17 日的午餐</p>
-          <p>主厨静候您的光临</p>
-        </div>
+      
+      <div class="btns" :class="{ btnTop: !state }">
+        <btn :text='btnText1' :type='1' :url='url1'/>  
+        <btn v-if='state <= 1' :text='btnText2' :type='2' :url='url2' class="mt24"/>  
       </div>
     </div>
     <side :sideShow='sideShow' v-on:hideSide='showSide'/>
@@ -27,6 +34,7 @@
 
 <script>
 import side from '@/components/side/side.vue'
+import btn from '@/components/btn/btn.vue'
 
 // state 为客户状态
 // 0 初次登录，未约时间，未点菜
@@ -38,16 +46,42 @@ export default {
   data() {
     return {
       state: 0,
-      sideShow: false
+      sideShow: false,
+      btnText1: '',
+      btnText2: '',
+      url1: '/home',
+      url2: '/home',
     }
+  },
+  computed: {
+  },
+  created() {
+    this.stateChange();
   },
   methods: {
     showSide() {
       this.sideShow = !this.sideShow;
+    },
+    stateChange() {
+      if (this.state === 0) {
+        this.btnText1 = '预约';
+        this.btnText2 = '菜单';
+        this.url1 = '/plan';
+        this.url2 = '/menu';
+      } else if (this.state === 1) {
+        this.btnText1 = '餐厅位置';
+        this.btnText2 = '联系主厨';
+        this.url1 = '/home';
+        this.url2 = '/home';
+      } else if (this.state === 2) {
+        this.btnText1 = '结账';
+        this.url1 = '/home';
+      }
     }
   },
   components:{
-    side
+    side,
+    btn
   }
 }
 </script>
@@ -68,8 +102,10 @@ export default {
     right: 0;
     bottom: 0;
     left: 0;
-    padding: 56px 0 0 24px;
     background: linear-gradient(to bottom, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 1));
+    .pdinner {
+      padding: 56px 0 0 24px;
+    }
   }
   .nav {
     width: 24px;
@@ -95,10 +131,30 @@ export default {
   }
   .content {
     margin: 60px 0 0 36px;
+    letter-spacing: 4px;
     .hi {
-      line-height: 76px;
+      line-height: 46px;
       font-size: 30px;
       color: white;
+    }
+    .text {
+      line-height: 56px;
+      font-size: 24px;
+      color: white;
+      p {
+        margin: 0;
+        .time {
+          padding: 0 10px 2px;
+          vertical-align: middle;
+          background: $btn-orange;
+        }
+      }
+    }
+  }
+  .btns {
+    margin-top: 80px;
+    &.btnTop {
+      margin-top: 306px;
     }
   }
 }
